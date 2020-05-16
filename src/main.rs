b43e6ab6236ea946213fa32eb1f9e69120071bf6
chaosannals <chaosannals@outlook.com>
2020-05-16 11:20:31 +0800

    server

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ extern crate rand;
 
 use rand::prelude::*;
 use rand::distributions::{Uniform};
+use actix_web::{get, web, App, HttpServer, Responder};
 
 fn make_set(length: i32) -> Vec<i32> {
-    let mut r: Vec<i32> = vec![];
+    let mut r = vec![];
     let u = Uniform::new(0, 10000000);
     for _ in 0..length {
         r.push(u.sample(&mut rand::thread_rng()));
@@ -12,7 +13,17 @@ fn make_set(length: i32) -> Vec<i32> {
     return r;
 }
 
-fn main() {
-    let a: Vec<i32> = make_set(1000000);
-    let b: Vec<i32> = make_set(1000000);
+#[get("/{id}/{name}/index.html")]
+async fn index(info: web::Path<(u32, String)>) -> impl Responder {
+    let a: Vec<i32> = make_set(10000);
+    format!("Hello {0}! id:{1} => {2:?}", info.1, info.0, a)
 }
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
+
