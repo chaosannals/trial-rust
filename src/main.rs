c62d46569283eac2e6df95856d48f61821c05b45
chaosannals <chaosannals@outlook.com>
2020-05-30 10:43:38 +0800

    ip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate rand;
 
-use rand::prelude::*;
-use rand::distributions::{Uniform};
+include!("ip.rs");
+
 use actix_web::{get, web, App, HttpServer, Responder};
+use rand::distributions::Uniform;
+use rand::prelude::*;
 
 fn make_set(length: i32) -> Vec<i32> {
     let mut r = vec![];
@@ -14,16 +16,21 @@ fn make_set(length: i32) -> Vec<i32> {
 }
 
 #[get("/{id}/{name}/index.html")]
-async fn index(info: web::Path<(u32, String)>) -> impl Responder {
+async fn user_index(info: web::Path<(u32, String)>) -> impl Responder {
     let a: Vec<i32> = make_set(10000);
     format!("Hello {0}! id:{1} => {2:?}", info.1, info.0, a)
 }
 
+#[get("/ip.html")]
+async fn index() -> impl Responder {
+    let ip = get_wan_ip().await;
+    format!("Ip {0}", ip.unwrap())
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(user_index).service(index))
         .bind("127.0.0.1:8080")?
         .run()
         .await
 }
-
