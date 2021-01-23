e72df19e800f09aa4ecae0ed9795e71bab8bce6b
chaosannals <chaosannals@outlook.com>
2021-01-23 13:58:09 +0800

    sqlite

--- Readme.md
+++ Readme.md
@@ -1,1 +1,9 @@
 # [trial-rust](https://github.com/chaosannals/trial-rust)
+
+```bash
+# storage
+cargo run --bin trail-rust-storage
+
+# hostsman
+cargo run --bin trail-rust-hostsman
+```
---
+++ storage/src/main.rs
@@ -0,0 +1,32 @@
+use async_std::task;
+use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode};
+use sqlx::{ConnectOptions, Executor};
+use std::str::FromStr;
+
+async fn create_tables() -> Result<(), sqlx::Error> {
+    let mut conn = SqliteConnectOptions::from_str("sqlite:trail.db")?
+        .journal_mode(SqliteJournalMode::Wal)
+        .create_if_missing(true)
+        .connect()
+        .await?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS t_test (
+        id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+        name VARCHAR(50) 
+    )",
+    )
+    .await?;
+    Ok(())
+}
+
+async fn do_it() {
+    let s = match create_tables().await {
+        Ok(i) => format!("Ok {:?}", i),
+        Err(e) => format!("Err {:?}", e),
+    };
+    println!("{0}", s)
+}
+
+fn main() {
+    task::block_on(do_it());
+}
