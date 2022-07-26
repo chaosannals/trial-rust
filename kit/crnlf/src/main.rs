8b184b44e8d519d4885eb0c1b37c424ba84a9459
chaosannals <chaosannals@outlook.com>
2022-07-26 16:41:07 +0800

    save

--- kit/crnlf/src/main.rs
+++ kit/crnlf/src/main.rs
@@ -7,28 +7,38 @@ struct Opts {
     target: String,
     #[clap(short='m', long="mode", default_value="crlf2lf")]
     mode: String,
+    #[clap(short='e', long="is_print_error", value_parser, default_value_t=false)]
+    is_print_error: bool
 }
 
 fn replace_file(p: path::PathBuf, src: &str, dist: &str) -> io::Result<()> {
-    let mut f = fs::File::open(p.clone())?;
+    let mut f = fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .append(false)
+        .open(p.clone())?;
     let mut content = String::new();
     let s = f.read_to_string(&mut content)?;
     let nc = content.replace(src, dist);
     f.write_all(nc.as_bytes())?;
+    f.flush()?;
     println!("replace: {} {}", s, p.display());
     Ok(())
 }
 
-fn replace_dir(dirpath: path::PathBuf, src: &str, dist: &str) -> io::Result<()> {
+fn replace_dir(dirpath: path::PathBuf, src: &str, dist: &str, is_print_error: bool) -> io::Result<()> {
     for entry in fs::read_dir(dirpath)? {
         let p = entry?.path();
-        //println!("p: {}", p.display());
         if p.is_dir() && !p.ends_with(".") {
-            replace_dir(p, src, dist)?;
+            replace_dir(p.clone(), src, dist, is_print_error)?;
         } else if p.is_file() {
-            match replace_file(p, src, dist) {
+            match replace_file(p.clone(), src, dist) {
                 Ok(_) => (),
-                Err(e) => println!("error: {:?}", e),
+                Err(e) => {
+                    if is_print_error {
+                        println!("error: {} {:?}", p.display(), e)
+                    }
+                },
             }
         }
     }
@@ -39,13 +49,14 @@ fn replace_dir(dirpath: path::PathBuf, src: &str, dist: &str) -> io::Result<()> 
 fn main() {
     let opts: Opts = Opts::parse();
     let tdir = path::PathBuf::from(opts.target);
+    let is_print_error = opts.is_print_error;
     let r = match opts.mode.as_str() {
-        "crlf2lf" => replace_dir(tdir, "\r\n", "\n"),
-        "crlf2cr" => replace_dir(tdir, "\r\n", "\r"),
-        "lf2crlf" => replace_dir(tdir, "\n", "\r\n"),
-        "lf2cr" => replace_dir(tdir, "\n", "\r"),
-        "cr2crlf" => replace_dir(tdir, "\r", "\r\n"),
-        "cr2lf" => replace_dir(tdir, "\r", "\n"),
+        "crlf2lf" => replace_dir(tdir, "\r\n", "\n", is_print_error),
+        "crlf2cr" => replace_dir(tdir, "\r\n", "\r", is_print_error),
+        "lf2crlf" => replace_dir(tdir, "\n", "\r\n", is_print_error),
+        "lf2cr" => replace_dir(tdir, "\n", "\r", is_print_error),
+        "cr2crlf" => replace_dir(tdir, "\r", "\r\n", is_print_error),
+        "cr2lf" => replace_dir(tdir, "\r", "\n", is_print_error),
         _ => Ok({ println!("unknown mode"); }),
     };
     match r {
