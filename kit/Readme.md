7cea76c7a812a0b0a09eac5d920efb2762f40d1d
chaosannals <chaosannals@outlook.com>
2022-07-27 14:33:46 +0800

    move

--- kit/Readme.md
+++ kit/Readme.md
@@ -4,4 +4,8 @@
 # 替换换行符工具
 cargo run --bin crnlf
 cargo build --bin crnlf --release
+
+# mkpw
+cargo run --bin mkpw
+cargo build --bin mkpw --release
 ```
---
+++ kit/mkpw/src/main.rs
@@ -0,0 +1,21 @@
+use clap::Parser;
+use rand::prelude::*;
+
+const CHARSET: &'static str = "1234567890_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+#[derive(Parser, Debug)]
+struct Opts {
+    #[clap(short='l', long="length", default_value="16")]
+    length: usize,
+}
+
+fn main() {
+    let opts: Opts = Opts::parse();
+    let charlen = CHARSET.len();
+    let mut rng = thread_rng();
+    for _ in 0..opts.length {
+        let n = rng.gen_range(0..charlen);
+        let c = CHARSET.chars().nth(n);
+        print!("{0}", c.unwrap());
+    }
+}
