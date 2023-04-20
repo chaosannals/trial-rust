454897f10fffef847a60a4c5dfca67fc276f7475
chaosannals <chaosannals@outlook.com>
2023-04-20 17:57:57 +0800

    save

--- slintdemo/slintdemo/src/main.rs
+++ slintdemo/slintdemo/src/main.rs
@@ -1,6 +1,7 @@
-// 官方提供 2种方式，以下是第一种。
+// 官方提供 2种方式，以下是 第一种 内联宏方式。
 // 不可以通过编译，要使用 *.slint 的方式
 // 注：详见 slinttdemo 代码，使用第二种方式。
+// 内联宏感觉不错，直接通过宏实现语法分析，内置新语言。
 
 slint::slint!{
     export component HelloWorld {
@@ -12,5 +13,5 @@ slint::slint!{
 }
 
 fn main() {
-    HelloWorld::new().run();
+    HelloWorld::new().unwrap().run().unwrap();
 }
--- slintdemo/slinttdemo/src/main.rs
+++ slintdemo/slinttdemo/src/main.rs
@@ -1,13 +1,20 @@
+use slint::SharedString;
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
     let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
+    ui.on_to_page(move | e | {
         let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+        ui.set_tip(e);
     });
+    ui.invoke_to_page(SharedString::from("CCC"));
+    // ui.on_request_increase_value(move || {
+    //     let ui = ui_handle.unwrap();
+    //     ui.set_counter(ui.get_counter() + 1);
+    // });
 
     ui.run()
 }
