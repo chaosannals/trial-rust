55633c3b9c2bccef3a8ca8ccc03caafbf0c08746
chaosannals <chaosannals@outlook.com>
2023-05-16 17:24:11 +0800

    save

--- logsrv/tcpclient/src/main.rs
+++ logsrv/tcpclient/src/main.rs
@@ -1,9 +1,30 @@
-use std::{io, str, io::Write, io::Read};
+use std::{
+    io,
+    str,
+    io::{
+        Write,
+        Read,
+        Result,
+    },
+};
 use std::net::{TcpStream};
+use aes_gcm_siv::{
+    Error,
+    aead::{AeadInPlace, KeyInit, OsRng, Aead},
+    Aes256GcmSiv, Nonce
+};
+use generic_array::{
+    GenericArray,
+    ArrayLength,
+};
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<()> {
+    // let key  = Aes256GcmSiv::generate_key(&mut OsRng);
+    let key = GenericArray::from_slice(&[1u8; 32]);
+    let cipher = Aes256GcmSiv::new(&key);
+    let nonce = Nonce::from_slice(b"unique nonce");
+
     let mut stream = TcpStream::connect("127.0.0.1:33333")?;
-
     let mut buf = [0u8; 128];
     loop {
         io::stdout().write_all(b"send: ")?;
@@ -11,8 +32,21 @@ fn main() -> std::io::Result<()> {
         let mut input = String::new();
         io::stdin().read_line(&mut input)?; // 空输入会导致接收方读不到。TODO 禁止空输入
         let d = input.trim().as_bytes();
-        stream.write(&d)?;
-        let n = stream.read(&mut buf[0..128])?;
-        io::stdout().write_all(&buf[0..n])?;
+        if d.len() > 0 {
+            // let mut buffer: Vec<u8> = Vec::new();
+            // buffer.extend_from_slice(&d);
+            // cipher.encrypt_in_place(nonce, b"", &mut buffer)?;
+
+            // 这个算法用了很奇怪的 Result ，和一般的 Result 不兼容，还要自己实现 Error
+            // 直接 expect 处理了。 TODO 错误处理
+            let ciphertext = cipher.encrypt(nonce, d).expect("encrypt error");
+            stream.write(&ciphertext)?;
+
+            // stream.write(&d)?;
+            let n = stream.read(&mut buf[0..128])?;
+            io::stdout().write_all(&buf[0..n])?;
+        } else {
+            io::stdout().write_all(b"empty")?;
+        }
     }
 }
--- logsrv/tcpserver/src/main.rs
+++ logsrv/tcpserver/src/main.rs
@@ -1,30 +1,49 @@
 use std::io::prelude::*;
 use std::io::{stdout};
 use std::thread::{spawn};
+use std::error::Error;
 use std::net::{TcpListener, TcpStream};
+use aes_gcm_siv::{
+    aead::{AeadInPlace, KeyInit, OsRng, Aead},
+    Aes256GcmSiv, Nonce
+};
+use generic_array::{
+    GenericArray,
+    ArrayLength,
+};
+
 
 fn main() -> std::io::Result<()> {
     let address = "0.0.0.0:33333";
     let listener = TcpListener::bind(address)?;
-    //listener.set_nonblocking(true).expect("Cannot set non-blocking");
+    // listener.set_nonblocking(true).expect("Cannot set non-blocking");
 
     for stream in listener.incoming() {
         // 此处是同步的，标准库没有异步库，需要使用第三方。
         // 此处读写最好另外起线程或者协程，防止阻塞。
         match stream {
             Ok(mut s) => {
+                // let handle = spawn(move || {
                 spawn(move || {
-                    let b = &mut [0; 128];
+                    let key = GenericArray::from_slice(&[1u8; 32]);
+                    let cipher = Aes256GcmSiv::new(&key);
+                    let nonce = Nonce::from_slice(b"unique nonce");
+
                     loop {
+                        let b = &mut [0; 32];
                         let n = s.read(b).expect("read error");
-                        // stdout().write_all(&b[0..n]).expect("write error"); // std 写入无效
-                        s.write(b"linked").expect("read error");
-                        // stdout().write_all(b"end").expect("write error"); // std 写入无效
+
+                        let text = cipher.decrypt(nonce, &b[0..n]).expect("decrypt error");
+                        s.write(&text).expect("read error");
+
+                        // stdout().lock().write_all(&text).expect("write error"); // std 写入无效
+                        // stdout().lock().flush();
                     }
                 });
+                // handle.join().expect("join error");
             }
-            Err(_e) => {
-                stdout().write_all(b"error")?;
+            Err(e) => {
+                stdout().write_all(format!("error: {:?}", e).as_bytes())?;
             }
         }
     }
