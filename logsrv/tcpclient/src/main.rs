9d293273d72a84fff858bdca8e32b5487b6fb208
chaosannals <chaosannals@outlook.com>
2023-05-20 09:01:23 +0800

    save

--- logsrv/tcpclient/src/main.rs
+++ logsrv/tcpclient/src/main.rs
@@ -6,10 +6,11 @@ use std::{
         Read,
         Result,
     },
+    result,
 };
 use std::net::{TcpStream};
+use std::error::Error;
 use aes_gcm_siv::{
-    Error,
     aead::{AeadInPlace, KeyInit, OsRng, Aead},
     Aes256GcmSiv, Nonce
 };
@@ -30,26 +31,33 @@ fn main() -> Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:33333")?;
     let mut buf = [0u8; 128];
     loop {
-        io::stdout().write_all(b"send: ")?;
-        io::stdout().flush()?;
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?; // 空输入会导致接收方读不到。TODO 禁止空输入
-        let d = input.trim().as_bytes();
-        if d.len() > 0 {
-            // let mut buffer: Vec<u8> = Vec::new();
-            // buffer.extend_from_slice(&d);
-            // cipher.encrypt_in_place(nonce, b"", &mut buffer)?;
+        let result: result::Result<(),Box<dyn Error>> = {
+            io::stdout().write_all(b"send: ")?;
+            io::stdout().flush()?;
+            let mut input = String::new();
+            io::stdin().read_line(&mut input)?; // 空输入会导致接收方读不到。TODO 禁止空输入
+            let d = input.trim().as_bytes();
+            if d.len() > 0 {
+                // let mut buffer: Vec<u8> = Vec::new();
+                // buffer.extend_from_slice(&d);
+                // cipher.encrypt_in_place(nonce, b"", &mut buffer)?;
 
-            // 这个算法用了很奇怪的 Result ，和一般的 Result 不兼容，还要自己实现 Error
-            // 直接 expect 处理了。 TODO 错误处理
-            let ciphertext = cipher.encrypt(nonce, d).expect("encrypt error");
-            stream.write(&ciphertext)?;
+                // 这个算法用了很奇怪的 Result ，和一般的 Result 不兼容，还要自己实现 Error
+                // 直接 expect 处理了。 TODO 错误处理
+                let ciphertext = cipher.encrypt(nonce, d).expect("encrypt error");
+                stream.write(&ciphertext)?;
 
-            // stream.write(&d)?;
-            let n = stream.read(&mut buf[0..128])?;
-            io::stdout().write_all(&buf[0..n])?;
-        } else {
-            io::stdout().write_all(b"empty")?;
+                // stream.write(&d)?;
+                let n = stream.read(&mut buf[0..128])?;
+                io::stdout().write_all(&buf[0..n])?;
+            } else {
+                io::stdout().write_all(b"empty")?;
+            }
+            Ok(())
+        };
+        match result {
+            Ok(()) => io::stdout().write_all(b"ok")?,
+            Err(e) => io::stdout().write_all(format!("error: {:?}", e).as_bytes())?,
         }
     }
 }
--- logsrv/tcpserver/src/main.rs
+++ logsrv/tcpserver/src/main.rs
@@ -1,9 +1,11 @@
 use std::io::prelude::*;
 use std::io::{stdout};
 use std::thread::{spawn};
-use std::error::Error;
+use std::error;
+use std::result;
 use std::net::{TcpListener, TcpStream};
 use aes_gcm_siv::{
+    Error,
     aead::{AeadInPlace, KeyInit, OsRng, Aead},
     Aes256GcmSiv, Nonce
 };
@@ -12,6 +14,29 @@ use generic_array::{
     ArrayLength,
 };
 
+fn handle_loop(s: &mut TcpStream) -> result::Result<(), Box<dyn error::Error>> {
+    let pass = b"pass";
+    let mut keybuf = [0u8; 32];
+    keybuf[0..pass.len()].copy_from_slice(&pass[..]);
+    let key = GenericArray::from_slice(&keybuf);
+    let cipher = Aes256GcmSiv::new(&key);
+    let nonce = Nonce::from_slice(b"unique nonce");
+
+    loop {
+        let r : result::Result<(), Box<dyn error::Error>> = {
+            let b = &mut [0; 32];
+            let n = s.read(b)?;
+
+            // TODO 这个的 Result 很奇怪，实现不知道放哪个包了。
+            let text = cipher.decrypt(nonce, &b[0..n]).expect("decrypt error");
+            s.write(&text)?;
+
+            // stdout().lock().write_all(&text).expect("write error"); // std 写入无效
+            // stdout().lock().flush();
+            Ok(())
+        };
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let address = "0.0.0.0:33333";
@@ -23,27 +48,9 @@ fn main() -> std::io::Result<()> {
         // 此处读写最好另外起线程或者协程，防止阻塞。
         match stream {
             Ok(mut s) => {
-                // let handle = spawn(move || {
                 spawn(move || {
-                    let pass = b"pass";
-                    let mut keybuf = [0u8; 32];
-                    keybuf[0..pass.len()].copy_from_slice(&pass[..]);
-                    let key = GenericArray::from_slice(&keybuf);
-                    let cipher = Aes256GcmSiv::new(&key);
-                    let nonce = Nonce::from_slice(b"unique nonce");
-
-                    loop {
-                        let b = &mut [0; 32];
-                        let n = s.read(b).expect("read error");
-
-                        let text = cipher.decrypt(nonce, &b[0..n]).expect("decrypt error");
-                        s.write(&text).expect("read error");
-
-                        // stdout().lock().write_all(&text).expect("write error"); // std 写入无效
-                        // stdout().lock().flush();
-                    }
+                    handle_loop(&mut s);
                 });
-                // handle.join().expect("join error");
             }
             Err(e) => {
                 stdout().write_all(format!("error: {:?}", e).as_bytes())?;
