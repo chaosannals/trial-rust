41bc147d13f136975f5ed41174fcfbdfeeaff503
chaosannals <chaosannals@outlook.com>
2023-07-10 09:31:02 +0800

    save

--- dbormdemo/seaormdemo/Readme.md
+++ dbormdemo/seaormdemo/Readme.md
@@ -1,3 +1,35 @@
 # sea-orm
 
 Sqlite 不会自动创建数据库文件。
+
+注：外部有个 seaormdemo 是 actix-web 的。
+
+```bash
+# 安装命令行工具
+cargo install sea-orm-cli
+
+# 迁移命令查看
+sea-orm-cli migrate -h
+
+# 初始化迁移，在项目里执行会多出迁移项目。（只是复制出一个模板，没有自动化项目配置，目前是个半残命令）
+sea-orm-cli migrate init
+
+# 创建迁移脚本文件。此脚本会自动更新 lib.rs 文件。
+sea-orm-cli migrate generate create_bakery_table
+sea-orm-cli migrate generate create_chef_table
+```
+
+```bash
+# Linux
+export DATABASE_URL="sqlite:./test.db"
+sea-orm-cli migrate refresh
+```
+
+```bat
+@rem Windows cmd 执行，不过路径是相对临时文件的。指定绝对路径。
+@rem 必须是 cmd ，在 powershell 下 set 无效。
+@rem cmd 不能加双引号，路径还必须是绝对的。很不好用。
+@rem 这个框架没有好好适配 Windows，这样 Linux 和 Windows 配置不能一致。不过执行可以通过。
+set DATABASE_URL=sqlite://D:\t1.db
+sea-orm-cli migrate refresh
+```
---
+++ dbormdemo/seaormdemo/migration/src/m20230710_011100_create_bakery_table.rs
@@ -0,0 +1,43 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    // Define how to apply this migration: Create the Bakery table.
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Bakery::Table)
+                    .col(
+                        ColumnDef::new(Bakery::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Bakery::Name).string().not_null())
+                    .col(ColumnDef::new(Bakery::ProfitMargin).double().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    // Define how to rollback this migration: Drop the Bakery table.
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Bakery::Table).to_owned())
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+pub enum Bakery {
+    Table,
+    Id,
+    Name,
+    ProfitMargin,
+}
