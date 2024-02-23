a96fe07d6df124b75fa7cb83d67960ec2e9328cb
chaosannals <chaosannals@outlook.com>
2024-02-23 15:21:26 +0800

    save

--- crossdemo/hwdemo/src/main.rs
+++ crossdemo/hwdemo/src/main.rs
@@ -1,6 +1,21 @@
-fn main() {
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    
-    println!("Hello, world!");
-    log::info!("log start");
+
+    let port = 40404;
+    log::info!("start hw at port: {:?}", port);
+
+    HttpServer::new(|| {
+        App::new().service(greet)
+    })
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await
 }
