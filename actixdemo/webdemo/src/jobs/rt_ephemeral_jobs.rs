19447930be050245d601faa0673023912a76a58e
chaosannals <chaosannals@outlook.com>
2024-03-01 15:03:12 +0800

    save

--- actixdemo/webdemo/src/jobs/rt_ephemeral_jobs.rs
+++ actixdemo/webdemo/src/jobs/rt_ephemeral_jobs.rs
@@ -18,15 +18,30 @@ pub fn start_queue() -> (JobsQueue, JoinHandle<()>) {
 
     (
         Arc::clone(&queue),
-        rt::spawn(async {
+        rt::spawn(async  move {
             loop {
-                if let Ok(job) = queue.try_lock() {
-                    log::info!("worker do");
-                    rt::time::sleep(Duration::from_millis(10000)).await
+                if let Ok(mut q) = queue.try_lock() {
+                    log::info!("worker lock");
+                    if let Some(job) = q.pop_back() {
+                        log::info!("job lock");
+                    } else {
+                        log::info!("not job, sleep.");
+                        rt::time::sleep(Duration::from_millis(10000)).await
+                    }
                 }
             }
         })
     )
 }
 
-pub 
+pub trait RteJob {
+    fn start(&mut self, item: JobItem);
+}
+
+impl RteJob for JobsQueue {
+    fn start(&mut self, item: JobItem) {
+        if let Ok(mut q) = self.try_lock() {
+            q.push_back(item);
+        }
+    }
+}
