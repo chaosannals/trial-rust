4a3aa9e6988d04346d0f61c188fe2c741b99daed
chaosannals <chaosannals@outlook.com>
2024-03-07 09:34:05 +0800

    save

--- actixdemo/libdemo/src/lib.rs
+++ actixdemo/libdemo/src/lib.rs
@@ -3,17 +3,15 @@ use std::{
 };
 
 pub fn get_self_exe_dir() -> String {
-    let exe_path_str = env::current_exe().unwrap();
-    let exe_path = Path::new(&exe_path_str);
+    let exe_path = env::current_exe().unwrap();
     let exe_dir = exe_path.parent().unwrap();
     log::info!("current exe dir: {:?}", exe_dir);
     exe_dir.to_string_lossy().to_string()
 }
 
 pub fn get_self_exe_dir_path() -> PathBuf {
-    let exe_path_str = env::current_exe().unwrap();
-    let exe_path = Path::new(&exe_path_str);
-    let exe_dir = exe_path.parent().unwrap();
-    log::info!("current exe dir: {:?}", exe_dir);
-    PathBuf::from(exe_dir)
+    let mut exe_path = env::current_exe().unwrap();
+    exe_path.pop();
+    log::info!("current exe dir: {:?}", exe_path);
+    exe_path
 }
