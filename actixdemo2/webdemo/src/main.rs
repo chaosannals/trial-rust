13471ec196a447e6bf417628e559df625e3b0f74
chaosannals <chaosannals@outlook.com>
2024-03-08 17:49:56 +0800

    save

--- actixdemo2/webdemo/src/main.rs
+++ actixdemo2/webdemo/src/main.rs
@@ -1,6 +1,9 @@
 use chrono::Local;
+use env_logger::Builder;
 
 fn main() {
+    Builder::new()
+        .init();
     let now = Local::now().naive_local();
-    println!("now: {:?}", now);
+    log::info!("now: {:?}", now);
 }
