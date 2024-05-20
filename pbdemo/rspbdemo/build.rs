0ce7c099bb6f84c6804f31e97ca5ad1793472372
chenshenchao <chenshenchao@outlook.com>
2024-05-21 02:06:26 +0800

    save.

--- pbdemo/rspbdemo/build.rs
+++ pbdemo/rspbdemo/build.rs
@@ -5,6 +5,8 @@ fn main() {
     let echo_proto_path = protos_dir.join("echo.proto");
 
     protobuf_codegen::Codegen::new()
+        // .protoc() // 可以使用 protoc
+        .pure() // 用纯 rust 实现替代 protoc
         .include(protos_dir)
         .inputs([echo_proto_path])
         .cargo_out_dir("rust_protobuf_protos")
