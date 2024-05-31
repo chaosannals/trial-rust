74e3c44456b93e7485d18febed970fb1ba4faf49
chaosannals <chaosannals@outlook.com>
2024-05-31 10:39:04 +0800

    save

--- pdfdemo/pdfdemo/src/main.rs
+++ pdfdemo/pdfdemo/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::current_dir;
+use anyhow::Result;
+use image::io::Reader as ImageReader;
+
+fn main() -> Result<()> {
+    let here = current_dir()?;
+    let png_path = here.join("test.png");
+    println!("here: {:?}", png_path);
+    let img = ImageReader::open(png_path)?.decode()?;
+    img.save("test.tmp.png")?;
+    Ok(())
 }
